9ca9cdb361a7c36793ca4487dec2720e5d3916ea
rgbkids <rgb@rgb-kids.com>
2015-05-31 10:44:07 +0900

    test

--- src/Auth.java
+++ src/Auth.java
@@ -1,5 +1,6 @@
+// commons-codec-1.10.jar
 import org.apache.commons.codec.digest.DigestUtils;
- 
+
 public class Auth {
 	public static void main(String[] args) {
 		System.out.println("SHA-256 = " + DigestUtils.sha256Hex("abc"));
